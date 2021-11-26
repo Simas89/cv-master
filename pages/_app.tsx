7cbbb7ec5fd6f424267341e73923524c4e9001a8
Simas <simaszurauskas@gmail.com>
2021-11-26 18:08:19 +0200

    dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import type { AppProps } from "next/app";
-import "regenerator-runtime/runtime";
-import theme from "../src/util/theme";
-import createEmotionCache from "../src/util/createEmotionCache";
-import { EmotionCache } from "@emotion/cache";
-import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { CacheProvider } from "@emotion/react";
-import { Provider } from "react-redux";
-import { store } from "state";
+import type { AppProps } from 'next/app';
+import 'regenerator-runtime/runtime';
+import theme from '../src/util/theme';
+import createEmotionCache from '../src/util/createEmotionCache';
+import { EmotionCache } from '@emotion/cache';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { CacheProvider } from '@emotion/react';
+import { Provider } from 'react-redux';
+import { store } from 'state';
+import Layout from 'components/Layout';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -24,16 +25,18 @@ const MyApp = ({
 }: AppPropsMod) => {
   return (
     <>
-      <CacheProvider value={emotionCache}>
-        <MuiThemeProvider theme={theme}>
+      <MuiThemeProvider theme={theme}>
+        <CacheProvider value={emotionCache}>
           <StyledThemeProvider theme={theme}>
             <CssBaseline />
             <Provider store={store}>
-              <Component {...pageProps} />
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
             </Provider>
           </StyledThemeProvider>
-        </MuiThemeProvider>
-      </CacheProvider>
+        </CacheProvider>
+      </MuiThemeProvider>
     </>
   );
 };

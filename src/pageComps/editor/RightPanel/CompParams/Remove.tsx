62db96d3ab9e9b895038ef13742acc754b24a577
Simas <simas.zurauskas@radicalcompany.com>
2021-12-24 10:07:55 +0200

    Remove.tsx clean up

--- src/pageComps/editor/RightPanel/CompParams/Remove.tsx
+++ src/pageComps/editor/RightPanel/CompParams/Remove.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useStateSelector } from 'state';
-import useCreateFreeSpace from 'hooks/useCreateFreeSpace';
 import useActionsInventory from 'state/actionHooks/useActionsInventory';
 import { Button } from '@mui/material';
-import isEqual from 'lodash.isequal';
 
 interface RemoveProps {
   pageId: string;
@@ -11,28 +8,9 @@ interface RemoveProps {
 }
 
 export const Remove: React.FC<RemoveProps> = ({ pageId, componentId }) => {
-  const componentsDimensions = useStateSelector(({ inventory }) => {
-    const comp = inventory.pages[pageId].components[componentId];
-    const height = comp.height;
-    const width = comp.width;
-    const hLocation = comp.hLocation;
-    const vLocation = comp.vLocation;
-
-    return [
-      {
-        height,
-        width,
-        hLocation,
-        vLocation,
-      },
-    ];
-  }, isEqual);
-
-  const setSpace = useCreateFreeSpace();
   const { deleteComponent, setSelectedComponent } = useActionsInventory();
 
   const removeComponent = () => {
-    setSpace({ isFree: true, pageId, componentsDimensions });
     deleteComponent({ pageId, componentId });
     setSelectedComponent({ pageId, componentId: '' });
   };
